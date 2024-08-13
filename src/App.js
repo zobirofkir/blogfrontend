b2d53a9d6ccc31b1fb8d8e482aeffd181a70db6c
zobirofkir <zobirofkir19@gmail.com>
2024-08-13 16:02:19 +0100

    Create Screen

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LayoutComponent from './Layout/LayoutComponent';
+
+// Import your pages here
+import HomeScreen from './Screens/HomeScreen';
+import AboutScreen from './Screens/AboutScreen';
+import BlogScreen from './Screens/BlogScreen';
+import NewsScreen from './Screens/NewsScreen';
+import ContactScreen from './Screens/ContactScreen';
 
 const App = () => {
   return (
-    <LayoutComponent>
-      <div>
-        {/* Main content goes here */}
-        <h1>Welcome to CSW-BLOG</h1>
-        <p>This is your main content area.</p>
-      </div>
-    </LayoutComponent>
+    <Router>
+      <LayoutComponent>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/about" element={<AboutScreen />} />
+          <Route path="/blog" element={<BlogScreen />} />
+          <Route path="/news" element={<NewsScreen />} />
+          <Route path="/contacts" element={<ContactScreen />} />
+        </Routes>
+      </LayoutComponent>
+    </Router>
   );
 }
 
