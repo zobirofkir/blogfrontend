aa52f40a53b68d76a9426905339c7cc431d422c3
zobirofkir <zobirofkir19@gmail.com>
2024-08-15 17:10:50 +0100

    Update Some Needs

--- src/Components/auth/FirstTimePopup.js
+++ src/Components/auth/FirstTimePopup.js
@@ -10,15 +10,10 @@ const FirstTimePopup = ({ onClose, user }) => {
           alt="Popup Icon"
           className="mx-auto mb-4"
         />
-        <h2 className="text-xl font-bold mb-2">Hello {user.name}</h2>
+        <h2 className="text-xl font-bold mb-2">Hello Admin</h2>
         <p className="text-gray-600 mb-4">
           Hey there! Now You Are Admin You Can Create A Blog But You Don't Have Access To User Management (For create and update and delete a user) .
         </p>
-        <h2 className="text-black mb-2 text-xl font-bold">
-          Your Info 
-        </h2>
-        <p>{user.name}</p>
-        <p>{user.email}</p>
         <button
           className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
           onClick={onClose}
