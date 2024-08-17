24ca6c1d1bcd0fb8fef4684b3d04451360e3d676
zobirofkir <zobirofkir19@gmail.com>
2024-08-17 01:59:26 +0100

    Update some needs

--- src/Screens/dashboard/DashboardScreen.js
+++ src/Screens/dashboard/DashboardScreen.js
@@ -405,7 +405,7 @@ const DashboardScreen = () => {
 
           {/* Project Cards Section */}
           <div className="mt-8">
-            <h2 className="text-2xl font-semibold dark:text-white">Project</h2>
+            <h2 className="text-2xl font-semibold dark:text-white">Projects</h2>
             <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
               {projects.map((project) => (
                 <div key={project.id} className="bg-white dark:bg-gray-800 p-4 rounded-lg shadow-md cursor-pointer" onClick={() => handleProjectClick(project)}>
